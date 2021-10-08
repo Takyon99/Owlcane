435a23d7c103e68ff27ec92a2d9e74a442ff9dda
Takyon99 <91360611+Takyon99@users.noreply.github.com>
2021-10-08 20:27:11 +1300

    Add intro window animation

--- Assets/Scripts/GreetingScript.cs
+++ Assets/Scripts/GreetingScript.cs
@@ -6,11 +6,12 @@ using DG.Tweening;
 
 public class GreetingScript : MonoBehaviour
 {
-
+    public GameObject window;
     public GameObject backButton;
     // Start is called before the first frame update
     void Start()
     {
+        window.transform.DOMoveY(transform.position.y + 1000, 2).SetUpdate(true);
         backButton.GetComponent<Button>().Select();
         Time.timeScale = 0;
     }
@@ -27,12 +28,16 @@ public class GreetingScript : MonoBehaviour
         StartCoroutine(Accept());
     }
 
+ 
+
     public IEnumerator Accept()
     {
         Tween pressIn = backButton.transform.DOScale(0.9f, 0.2f).SetUpdate(true);
         yield return pressIn.WaitForCompletion();
         Tween pressOut = backButton.transform.DOScale(1.0f, 0.2f).SetUpdate(true);
         yield return pressOut.WaitForCompletion();
+        Tween down = window.transform.DOMoveY(transform.position.y - 1000, 2).SetUpdate(true);
+        yield return down.WaitForCompletion();
         Time.timeScale = 1;
         this.gameObject.SetActive(false);
     }
